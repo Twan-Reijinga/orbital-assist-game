218e3b0021e9af3040b299893d7aa2f4a1e8fcb5
Twan Reijinga <twan.reijinga@coderclass.nl>
2022-07-08 10:16:35 +0200

    add draw function for planet

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
+let planet;
+let satilite;
+
+function preload() {}
+
 function setup() {
     const WIDTH = 700;
     const HEIGHT = 700;
     createCanvas(WIDTH, HEIGHT);
     frameRate(144);
-    new Planet();
-    new Satilite();
+    planet = new Planet(WIDTH / 2, HEIGHT / 2, 50, "red");
+    satilite = new Satilite();
+    noStroke();
 }
 
 function draw() {
     background(69);
+    planet.draw();
 }
