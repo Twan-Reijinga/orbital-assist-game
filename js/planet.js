7575a489cf63de7048f2fb4df64d962308cf21bf
Twan Reijinga <twan.reijinga@coderclass.nl>
2022-07-08 10:21:11 +0200

    add location function in plannets

--- js/planet.js
+++ js/planet.js
@@ -1,13 +1,17 @@
 class Planet {
     constructor(x, y, radius, color) {
-        this.x = x;
-        this.y = y;
         this.radius = radius;
         this.color = color;
+        this.changeLoc(x, y);
     }
 
     draw() {
         fill(this.color);
         circle(this.x, this.y, this.radius);
+    }
+
+    changeLoc(x, y) {
+        this.x = x;
+        this.y = y;
     }
 }
